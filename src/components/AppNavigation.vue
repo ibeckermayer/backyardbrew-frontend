8b30fbf34a3e7815e5587bc65b92930dde8dade5
ibeckermayer <ibeckermayer@gmail.com>
2019-03-10 23:17:24 -0700

    the beginnings of a proper toolbar

---
+++ src/components/AppNavigation.vue
@@ -0,0 +1,28 @@
+<template>
+    <v-toolbar app color="white" extended="true">
+        <v-container class="logo-container">
+                <v-layout align-center justify-start row fill-height>
+                    <v-flex>
+                        <img src="../assets/aoc_coffee.jpg" height="100px">
+                    </v-flex>
+                </v-layout>
+            </v-container>
+        <v-toolbar-items>
+            <v-btn flat>Link One</v-btn>
+            <v-btn flat>Link Two</v-btn>
+            <v-btn flat>Link Three</v-btn>
+        </v-toolbar-items>
+    </v-toolbar>
+</template>
+
+<script>
+export default {
+    name: 'AppNavigation'
+};
+</script>
+
+<style scoped>
+.logo-container{
+    padding: 0;
+}
+</style>
