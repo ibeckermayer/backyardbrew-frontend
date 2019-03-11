3431961859e031ce55832ea1dcbdcdefdac490e4
ibeckermayer <ibeckermayer@gmail.com>
2019-03-10 23:34:49 -0700

    best toolbar layout so far

--- src/components/AppNavigation.vue
+++ src/components/AppNavigation.vue
@@ -1,12 +1,12 @@
 <template>
-    <v-toolbar app color="white" extended="true">
+    <v-toolbar app color="white" extended="true" extension-height="36px">
         <v-container class="logo-container">
-                <v-layout align-center justify-start row fill-height>
-                    <v-flex>
-                        <img src="../assets/aoc_coffee.jpg" height="100px">
-                    </v-flex>
-                </v-layout>
-            </v-container>
+            <v-layout align-center justify-start row fill-height>
+                <v-flex>
+                    <img src="../assets/aoc_coffee.jpg" height="100px">
+                </v-flex>
+            </v-layout>
+        </v-container>
         <v-toolbar-items>
             <v-btn flat>Link One</v-btn>
             <v-btn flat>Link Two</v-btn>
@@ -24,5 +24,6 @@ export default {
 <style scoped>
 .logo-container{
     padding: 0;
+    padding-top: 
 }
 </style>
