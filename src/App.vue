5951cd8d371ec53c6deb39255a0199dd208d6230
ibeckermayer <ibeckermayer@gmail.com>
2019-03-15 01:43:11 -0700

    padding

--- src/App.vue
+++ src/App.vue
@@ -18,4 +18,7 @@ export default {
 </script>
 
 <style>
+.v-content{
+    padding: 0;
+}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,12 +2,18 @@
     <v-container fluid fill-height style="max-height:100vh;" class="home">
         <v-layout justify-center align-center column pa-5>
             <v-flex>
-            <div class="display-4 font-weight-black white--text text-xs-center">Backyard Brew</div>
-            <div class="display-3 white--text text-xs-center mb-5">Experience Flavors. Don't Expect Them.</div>
-            <div class="display-1 font-weight-bold white--text text-xs-center">Finally be a foodie at home with fresh, chef-prepared meals delivered daily to your door.</div>
-            <v-btn fab class="mt-5 brown darken-4">
-            <v-icon large color="white">expand_more</v-icon>
-            </v-btn>
+                <div style="background:#3E2723;opacity:0.8;">
+                    <div style="opacity:1.0;" class="display-4 font-weight-black white--text text-xs-center mb-3" >Backyard Brew</div>
+                </div>
+                
+                <div class="display-3 white--text text-xs-center" style="background:#3E2723;opacity:0.8;">Experience Flavors.</div>
+                <div class="display-3 white--text text-xs-center mb-5" style="background:#3E2723;opacity:0.8;">Don't Expect Them.</div>
+
+            </v-flex>
+            <v-flex>
+                <v-btn fab class="mt-5 brown darken-4">
+                    <v-icon large color="white">expand_more</v-icon>
+                </v-btn>
             </v-flex>
         </v-layout>
     </v-container>
@@ -25,6 +31,8 @@ export default {
     background-size: cover;
     width: 100%;
     padding:0;
+    background-position:center;
+    opacity: 1;
     
     /* height: 100%; */
     
