e277da8d0bfa829e435e36e108982f5832109897
ibeckermayer <ibeckermayer@gmail.com>
2019-03-14 23:55:08 -0700

    App is looking pretty responsive on mobile. Considering adding more blockchain, will consult with microservice AI. Over and out.

--- src/components/AppNavigation.vue
+++ src/components/AppNavigation.vue
@@ -1,45 +1,61 @@
 <template>
-    <v-container class="nav-bar" fluid pa-0> 
-        <v-layout align-center row fill-height>
-            <v-flex xs4 lg3 offset-md0 offset-lg1>
-                <v-container>
-                    <v-layout align-center row fill-height>
-                        <v-flex align-self-center>
-                            <a><img src="../assets/aoc_coffee.jpg" height="100px"></a>
-                        </v-flex>
-                    </v-layout>
-                </v-container>
-            </v-flex>
-            <v-flex xs8 align-self-center>
-                <v-container class="button-container">
-                    <v-layout align-center justify-space-around row fill-height>
-                        <v-flex xs2>
-                            <v-btn flat>Order Online</v-btn>
-                        </v-flex>
-                        <v-flex xs2>
-                            <v-btn flat>Brew</v-btn>
-                        </v-flex>
-                        <v-flex xs2>
-                            <v-btn flat>Bistro</v-btn>
-                        </v-flex>
-                        <v-flex xs2>
-                            <v-btn flat>Micro-Roasting</v-btn>
-                        </v-flex>
-                        <v-flex xs2>
-                            <v-btn flat>Retail</v-btn>
-                        </v-flex>
-                        <v-flex xs2>
-                            <v-btn flat>About</v-btn>
-                        </v-flex>
-                    </v-layout>
-                </v-container>
-            </v-flex>
-        </v-layout>
-    </v-container>
+    <span>
+        <v-container app hidden-md-and-up pa-0>
+            <v-toolbar color="#FFFFFF" light="">
+                <v-toolbar-side-icon @click="drawer = !drawer"></v-toolbar-side-icon>
+                <v-toolbar-title class="centered">ALLOUTCOMES COFFEE</v-toolbar-title>
+            </v-toolbar>
+        </v-container>
+        <v-navigation-drawer v-model="drawer" app>
+                    <p>test</p>
+                </v-navigation-drawer>
+        <v-container app hidden-sm-and-down class="nav-bar" fluid pa-0>
+            <v-layout align-center row fill-height>
+                <v-flex md4 lg3 offset-md0 offset-lg1>
+                    <v-container >
+                        <v-layout align-center row fill-height>
+                            <v-flex align-self-center>
+                                <a><img src="../assets/aoc_coffee.jpg" height="100px"></a>
+                            </v-flex>
+                        </v-layout>
+                    </v-container>
+                </v-flex>
+                <v-flex sm12 md8 align-self-center>
+                    <v-container class="button-container">
+                        <v-layout align-center justify-space-between row fill-height>
+                            <v-flex md2>
+                                <v-btn flat>Brew</v-btn>
+                            </v-flex>
+                            <v-flex md2>
+                                <v-btn flat>Bistro</v-btn>
+                            </v-flex>
+                            <v-flex md2>
+                                <v-btn flat>Micro-Roasting</v-btn>
+                            </v-flex>
+                            <v-flex md2>
+                                <v-btn flat>Retail</v-btn>
+                            </v-flex>
+                            <v-flex md2>
+                                <v-btn flat>About</v-btn>
+                            </v-flex>
+                            <v-flex md2>
+                                <v-btn flat>Order Online</v-btn>
+                            </v-flex>
+                        </v-layout>
+                    </v-container>
+                </v-flex>
+            </v-layout>
+        </v-container>
+    </span>
  </template>
 
 <script>
 export default {
+    data() {
+        return {
+            drawer:false
+        }
+    },
     name: 'AppNavigation'
 };
 </script>
