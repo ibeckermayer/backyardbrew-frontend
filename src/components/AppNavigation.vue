f2999a9751f933423faaa5a1c2d5ef1e5e56bf44
ibeckermayer <ibeckermayer@gmail.com>
2019-03-14 21:35:42 -0700

    made screen more responses to different window sizes

--- src/components/AppNavigation.vue
+++ src/components/AppNavigation.vue
@@ -1,7 +1,7 @@
 <template>
-    <v-container class="nav-bar"> 
+    <v-container class="nav-bar" fluid pa-0> 
         <v-layout align-center row fill-height>
-            <v-flex xs6>
+            <v-flex xs4 lg3 offset-md0 offset-lg1>
                 <v-container>
                     <v-layout align-center row fill-height>
                         <v-flex align-self-center>
@@ -13,20 +13,23 @@
             <v-flex xs8 align-self-center>
                 <v-container class="button-container">
                     <v-layout align-center justify-space-around row fill-height>
-                        <v-flex xs4>
+                        <v-flex xs2>
                             <v-btn flat>Order Online</v-btn>
                         </v-flex>
-                        <v-flex xs4>
+                        <v-flex xs2>
                             <v-btn flat>Brew</v-btn>
                         </v-flex>
-                        <v-flex xs4>
+                        <v-flex xs2>
                             <v-btn flat>Bistro</v-btn>
                         </v-flex>
-                        <v-flex xs4>
+                        <v-flex xs2>
                             <v-btn flat>Micro-Roasting</v-btn>
                         </v-flex>
-                        <v-flex xs4>
+                        <v-flex xs2>
                             <v-btn flat>Retail</v-btn>
+                        </v-flex>
+                        <v-flex xs2>
+                            <v-btn flat>About</v-btn>
                         </v-flex>
                     </v-layout>
                 </v-container>
