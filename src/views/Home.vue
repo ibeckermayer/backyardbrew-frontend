aaaae8e214b9fa76ccdb15ebfee7b4425132ab7c
ibeckermayer <ibeckermayer@gmail.com>
2019-03-15 01:55:07 -0700

    background overlay, need to figure out how to make lettering not opaque

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,13 @@
 <template>
     <v-container fluid fill-height style="max-height:100vh;" class="home">
-        <v-layout justify-center align-center column pa-5>
+        <v-layout style="background:#3E2723;opacity:0.5;" justify-center align-center column pa-5>
             <v-flex>
-                <div style="background:#3E2723;opacity:0.8;">
-                    <div style="opacity:1.0;" class="display-4 font-weight-black white--text text-xs-center mb-3" >Backyard Brew</div>
+                <div>
+                    <div class="display-4 font-weight-black white--text text-xs-center mb-3" style="opacity:1.0;">Backyard Brew</div>
                 </div>
                 
-                <div class="display-3 white--text text-xs-center" style="background:#3E2723;opacity:0.8;">Experience Flavors.</div>
-                <div class="display-3 white--text text-xs-center mb-5" style="background:#3E2723;opacity:0.8;">Don't Expect Them.</div>
+                <div class="display-3 white--text text-xs-center">Experience Flavors.</div>
+                <div class="display-3 white--text text-xs-center mb-5">Don't Expect Them.</div>
 
             </v-flex>
             <v-flex>
