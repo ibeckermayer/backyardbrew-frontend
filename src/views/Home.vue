29742a9aefcf63d4a347fbbd4d82ad4599edfd26
ibeckermayer <ibeckermayer@gmail.com>
2019-03-17 14:20:09 -0700

    figured out how to keep the overlay with an alpha, while having opacity=1 lettering

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,9 @@
 <template>
-    <v-container fluid fill-height style="max-height:100vh;" class="home">
-        <v-layout style="background:#3E2723;opacity:0.5;" justify-center align-center column pa-5>
+    <v-container fluid fill-height class="home">
+        <v-layout class="home-filter" justify-center align-center column pa-5>
             <v-flex>
                 <div>
-                    <div class="display-4 font-weight-black white--text text-xs-center mb-3" style="opacity:1.0;">Backyard Brew</div>
+                    <div class="display-4 font-weight-black white--text text-xs-center mb-3">Backyard Brew</div>
                 </div>
                 
                 <div class="display-3 white--text text-xs-center">Experience Flavors.</div>
@@ -30,11 +30,11 @@ export default {
     background: url('../assets/landing_coffee_0.jpg');
     background-size: cover;
     width: 100%;
-    padding:0;
+    height: 100%;
+    padding: 0;
     background-position:center;
-    opacity: 1;
-    
-    /* height: 100%; */
-    
+}
+.home-filter{
+    background:rgba(62, 39, 35, 0.3);
 }
 </style>
