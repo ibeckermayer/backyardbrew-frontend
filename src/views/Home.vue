144c461385c26b9b99229b80d1499e9b3f16291e
ibeckermayer <ibeckermayer@gmail.com>
2019-03-17 23:31:40 -0700

    changed the sizing of Backyard Brew lettering on small screens so it doesn't get pushed off the side

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,8 @@
         <v-layout class="home-filter" justify-center align-center column pa-5>
             <v-flex>
                 <div>
-                    <div class="display-4 font-weight-black white--text text-xs-center mb-3">Backyard Brew</div>
+                    <div class="hidden-sm-and-down display-4 font-weight-black white--text text-xs-center mb-3">Backyard Brew</div>
+                    <div class="hidden-md-and-up display-3 font-weight-black white--text text-xs-center mb-3">Backyard Brew</div>
                 </div>
                 <div class="display-3 white--text text-xs-center">Experience Flavors.</div>
                 <div class="display-3 white--text text-xs-center mb-5">Don't Expect Them.</div>
