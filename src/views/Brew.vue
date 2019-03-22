f606e64b6343f07881dcb65ac9fc2e01d1bfb879
ibeckermayer <ibeckermayer@gmail.com>
2019-03-22 00:04:35 -0700

    created brew page

--- src/views/Brew.vue
+++ src/views/Brew.vue
@@ -1,34 +1,35 @@
 <template>
-    <v-container fluid fill-height class="home">
-        <v-layout class="home-filter" justify-center column pa-5>
-            <HomeTitle></HomeTitle>
+    <v-container fluid fill-height class="brew">
+        <v-layout column wrap fill-height align-center="" justify-center="">
+            <v-flex class="img-container" md8 :class="{'pa-5': $vuetify.breakpoint.mdAndUp}">
+                <img src="../assets/brew_menu.png" width="100%">
+            </v-flex>
         </v-layout>
     </v-container>
+
 </template>
 
 <script>
-import HomeTitle from '../components/HomeTitle'
-import HomeCards from '../components/HomeCards'
-
 export default {
     name: 'Brew',
-    components: {
-        HomeTitle,
-        HomeCards
-    }
 };
 </script>
 
 <style scoped>
-.home {
-    background: url('../assets/landing_coffee_0.jpg');
+.brew {
+    background: url('../assets/landing_coffee_1.jpg');
     background-size: cover;
     width: 100%;
     height: 100%;
     padding: 0;
     background-position:center;
 }
-.home-filter{
-    background:rgba(62, 39, 35, 0.3);
+
+img {
+    opacity: 0.9;
+}
+
+.img-container {
+    flex: 0 1 auto;
 }
 </style>
