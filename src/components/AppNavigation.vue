b115013c4cdd6c2a34998835c4d152494dbeb051
ibeckermayer <ibeckermayer@gmail.com>
2019-03-24 22:00:15 -0700

    made button coloration brown themed

--- src/components/AppNavigation.vue
+++ src/components/AppNavigation.vue
@@ -39,25 +39,25 @@
                     <v-container class="button-container">
                         <v-layout align-center justify-space-around="" row fill-height>
                             <v-flex md1>
-                                <v-btn to="/brew" flat v-ripple="{ class: `brown--text` }">Brew</v-btn>
+                                <v-btn active-class="custom_button" to="/brew" flat v-ripple="{ class: `brown--text` }">Brew</v-btn>
                             </v-flex>
                             <v-flex md1>
-                                <v-btn to="/bistro" flat v-ripple="{ class: `brown--text` }">Bistro</v-btn>
+                                <v-btn active-class="custom_button" to="/bistro" flat v-ripple="{ class: `brown--text` }">Bistro</v-btn>
                             </v-flex>
                             <v-flex md1>
-                                <v-btn to="/roasting" flat v-ripple="{ class: `brown--text` }">Roasting</v-btn>
+                                <v-btn active-class="custom_button" to="/roasting" flat v-ripple="{ class: `brown--text` }">Roasting</v-btn>
                             </v-flex>
                             <v-flex md1>
-                                <v-btn to="/under_construction" flat v-ripple="{ class: `brown--text` }">About</v-btn>
+                                <v-btn active-class="custom_button" to="/under_construction" flat v-ripple="{ class: `brown--text` }">About</v-btn>
                             </v-flex>
                             <v-flex md1>
-                                <v-btn to="/under_construction" flat v-ripple="{ class: `brown--text` }">Retail</v-btn>
+                                <v-btn active-class="custom_button" to="/under_construction" flat v-ripple="{ class: `brown--text` }">Retail</v-btn>
                             </v-flex>
                             <v-flex md1>
-                                <v-btn to="/under_construction" flat v-ripple="{ class: `brown--text` }">R&D</v-btn>
+                                <v-btn active-class="custom_button" to="/under_construction" flat v-ripple="{ class: `brown--text` }">R&D</v-btn>
                             </v-flex>
                             <v-flex md1>
-                                <v-btn to="/under_construction" flat v-ripple="{ class: `brown--text` }">Tech</v-btn>
+                                <v-btn active-class="custom_button" to="/under_construction" flat v-ripple="{ class: `brown--text` }">Tech</v-btn>
                             </v-flex>
                             <v-flex md1>
                                 <v-btn color="#8D6E63" dark href="https://aoccoffee.com/collections/all" v-ripple="{ class: `brown--text` }">Order</v-btn>
@@ -90,15 +90,23 @@ export default {
                 },
                 {
                     nav_label: 'About',
-                    route: '/brew'
+                    route: '/under_construction'
                 },
                 {
                     nav_label: 'Retail',
-                    route: '/brew'
+                    route: '/under_construction'
                 },
                 {
-                    nav_label: 'Order Online',
-                    route: '/brew'
+                    nav_label: 'R&D',
+                    route: '/under_construction'
+                },
+                {
+                    nav_label: 'Tech',
+                    route: '/under_construction'
+                },
+                {
+                    nav_label: 'Order',
+                    route: '/under_construction'
                 },
             ]            
         }
@@ -129,7 +137,10 @@ a {
 }
 
 .v-btn:hover::before {
-    color: brown;
+    color: #8D6E63;
 }
 
+.custom_button {
+    background: rgba(141, 110, 99, 0.3);
+}
 </style>
