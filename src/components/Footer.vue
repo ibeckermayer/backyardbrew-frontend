5b038db76825b3ac12d859a398b349a9c1f73d3b
ibeckermayer <ibeckermayer@gmail.com>
2019-03-31 14:36:55 -0700

    make footer visible on mobile

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,29 +1,29 @@
 <template>
-    <v-container app hidden-sm-and-down class="footer" fluid pa-0 >
+    <v-container app class="footer" fluid pa-0 >
         <v-layout align-start row fill-height>
-            <v-flex md6>
+            <v-flex xs6>
                 <v-container fluid>
                     <v-layout column fill-height>
-                        <v-flex text-md-center md1>
+                        <v-flex text-xs-center xs1>
                             <h3>Partners</h3>
                         </v-flex>
-                        <v-flex text-md-center md1>
+                        <v-flex text-xs-center xs1>
                             <p>All photography by <a href="https://stefanradev.co/">Stefan Radev</a></p>
                         </v-flex>
                     </v-layout>
                 </v-container>
             </v-flex>
-            <v-flex md6>
+            <v-flex xs6>
                 <v-container fluid>
                     <v-layout column fill-height>
-                        <v-flex text-md-center md1>
+                        <v-flex text-xs-center xs1>
                             <a name="contact"></a>
                             <h3>Contact</h3>
                         </v-flex>
-                        <v-flex text-md-center md1>
+                        <v-flex text-xs-center xs1>
                             <p>ryankhalil91@gmail.com</p>
                         </v-flex>
-                        <v-flex text-md-center md1>
+                        <v-flex text-xs-center xs1>
                             <p>(650) 704-7785</p>
                         </v-flex>
                     </v-layout>
