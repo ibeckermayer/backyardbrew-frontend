d5d612c0b154b576eccd2a2dfed14d787be63773
ibeckermayer <ibeckermayer@gmail.com>
2019-03-31 01:32:08 -0700

    added functionality for viewing LoginDialog

--- src/components/AccountBar.vue
+++ src/components/AccountBar.vue
@@ -1,41 +1,47 @@
 <template>
-    <v-container app hidden-sm-and-down class="account-bar" fluid pa-0>
-        <v-layout align-center row fill-height>
-            <v-flex md12 elevation-1 class="account-bar-flex">
-                <v-container class="button-container" fluid>
-                    <v-layout justify-end row fill-height>
-                        <v-flex text-md-center md1>
-                            <a href="#contact">Contact</a>
-                        </v-flex>
-                        <RegisterDialogueLink></RegisterDialogueLink>
-                        <v-flex text-md-center md1>
-                            <a>Login</a>
-                        </v-flex>
-                        <v-flex text-md-center md1>
-                            <a>Cart</a>
-                        </v-flex>
-                        <v-flex text-md-center md1>
-                            <a>Account</a>
-                        </v-flex>
-                    </v-layout>
-                </v-container>
-            </v-flex>
-        </v-layout>
-    </v-container>
+    <span>
+        <v-container app hidden-sm-and-down class="account-bar" fluid pa-0>
+            <v-layout align-center row fill-height>
+                <v-flex md12 elevation-1 class="account-bar-flex">
+                    <v-container class="button-container" fluid>
+                        <v-layout justify-end row fill-height>
+                            <v-flex text-md-center md1>
+                                <a href="#contact">Contact</a>
+                            </v-flex>
+                            <v-flex text-md-center md1>
+                                <a @click.prevent="loginShow = true">Login</a>
+                            </v-flex>
+                            <v-flex text-md-center md1>
+                                <a>Register</a>
+                            </v-flex>
+                            <v-flex text-md-center md1>
+                                <a>Cart</a>
+                            </v-flex>
+                            <v-flex text-md-center md1>
+                                <a>Account</a>
+                            </v-flex>
+                        </v-layout>
+                    </v-container>
+                </v-flex>
+            </v-layout>
+        </v-container>
+
+        <LoginDialog v-bind:show='loginShow' @loginClose='loginShow = false'></LoginDialog>
+    </span>
 </template>
 
 <script>
-import RegisterDialogueLink from '@/components/RegisterDialogueLink'
+import LoginDialog from '@/components/LoginDialog'
 
 export default {
     name: 'AccountBar',
-    data () {
-      return {
-            dialog: false
-        }
-    },
     components: {
-        RegisterDialogueLink
+        LoginDialog,
+    },
+    data () {
+        return {
+            loginShow: false
+        }
     }
 }
 </script>
@@ -54,6 +60,7 @@ a {
     padding: 0;
     text-decoration: none;
     color: white;
+    font-weight: bold;
 }
 
 </style>
---
+++ src/components/LoginDialog.vue
@@ -0,0 +1,51 @@
+<template>
+    <v-dialog :value="show" @input="$emit('loginClose')" width="800">
+        <v-card>
+            <v-card-title>
+                <span class="headline">Login</span>
+                <v-spacer></v-spacer>
+
+            </v-card-title>
+            <v-card-text>
+                <v-container grid-list-md>
+                    <v-layout wrap>
+                        <v-flex xs12>
+                            <v-text-field color="primary" label="Email" required></v-text-field>
+                        </v-flex>
+                        <v-flex xs12>
+                            <v-text-field color="primary" label="Password" type="password" required></v-text-field>
+                        </v-flex>
+                    </v-layout>
+                </v-container>
+            </v-card-text>
+            <v-card-actions>
+            <v-container>
+                <v-layout justify-center align-center>
+            <v-btn color="primary" dark @click="$emit('loginClose')" text-md-center>Login</v-btn>
+                </v-layout>
+            </v-container>
+            </v-card-actions>
+        </v-card>
+    </v-dialog>
+</template>
+
+<script>
+
+export default {
+    name: 'LoginDialog',
+    props: ['show'],
+    data () {
+      return {
+        }
+    }
+}
+</script>
+
+<style scoped>
+a {
+    margin: 0;
+    padding: 0;
+    text-decoration: none;
+    color: white;
+}
+</style>
