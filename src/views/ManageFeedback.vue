79e630c15666f003720c10907eb01ff9010179e1
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2019-05-03 12:56:05 -0700

    semi-working layout of ManageFeedback vue, now turning cards into components

--- src/views/ManageFeedback.vue
+++ src/views/ManageFeedback.vue
@@ -1,5 +1,5 @@
 <template>
-    <v-container fluid fill-height class="under-const">
+    <!-- <v-container fluid fill-height class="under-const">
         <v-layout class="under-const-filter" justify-center column pa-5>
             <v-flex>
                 <div>
@@ -19,6 +19,35 @@
                 </div>
             </v-flex>
         </v-layout>
+    </v-container> -->
+    <!-- <ul>
+        <li v-for="(fb, index) in unresFeedback">
+            {{ fb.email }}
+        </li>
+    </ul> -->
+    <v-container>
+        <v-layout justify-center align-center="" column>
+            <v-flex>
+                Unresolved Feedback
+            </v-flex>
+            <v-flex ma-2 v-for="(fb, index) in unresFeedback">
+                <v-card width="800px">
+                    <v-card-title>
+                        <v-layout column>
+                            <v-flex><b>Name:</b> {{ fb.name }}</v-flex>
+                            <v-flex><b>Email:</b> {{ fb.email }}</v-flex>
+                            <v-flex><b>Recieved:</b> {{fb.rcvd_on}}</v-flex>
+                        </v-layout>
+                    </v-card-title>
+                    <v-card-text>
+                        {{ fb.text }}
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-btn small color="primary" outline="" @click="submit" text-xs-center>Mark Resolved</v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-flex>
+        </v-layout>
     </v-container>
 </template>
 
@@ -29,6 +58,12 @@ import router from '../router';
 
 export default {
     name: 'ManageFeedback',
+    data() {
+        return {
+            unresFeedback: [], // list of unresovled feedback object
+            resFeedback: []    // list of resolved feedback objects
+        }
+    },
     beforeRouteEnter (to, from, next) {
         const FEEDBACK_URL = process.env.VUE_APP_API_BASE_URL + '/feedback';
         let header = {
@@ -37,20 +72,23 @@ export default {
             }
         };
         console.log(header);
-        axios.post(FEEDBACK_URL,
+        axios.post(FEEDBACK_URL, // before entering route, get page 1 of unresolved feedbacks
         {
             resolved: false,
             page: 1
         },
         header)
-        .then(response => {
+        .then(response => { // if authorization succeeds
             console.log(response);
-            next();
+            next(vm => {
+                vm.unresFeedback = response.data.feedbacks; // fill unresFeedback with first page of unresolved Feedback
+                console.log(vm.unresFeedback);
+            });
         })
-        .catch(error => {
+        .catch(error => { // if authorization fails
             console.log(error.response.status);
             console.log(error.response.data);
-            router.push({path: '/unauthorized'});
+            router.push({path: '/unauthorized'}); // push user to unauthorized route
         });
     }
 };
